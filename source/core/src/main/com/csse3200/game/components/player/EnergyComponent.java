f01be5dd43cb1a30cb8dfe9fd032080da5f81513
Celeste116 <sixian.cheng@student.uq.edu.au>
2026-08-25 00:04:21 +1000

    refactor EnergyComponent with event notifications and proper setters

--- source/core/src/main/com/csse3200/game/components/player/EnergyComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/EnergyComponent.java
@@ -6,8 +6,12 @@ import org.slf4j.LoggerFactory;
 /**
  * Component for managing player energy and handling turn lifecycle triggers.
  */
-public class EnergyComponent {
+public class EnergyComponent extends Component {
     private static final Logger logger = LoggerFactory.getLogger(EnergyComponent.class);
+
+    private static final String EVT_UPDATE_ENERGY = "updateEnergy";
+    private static final String EVT_UPDATE_MAX_ENERGY = "updateMaxEnergy";
+
     private int currentEnergy;
     private int maxEnergy;
 
@@ -15,13 +19,25 @@ public class EnergyComponent {
         this.maxEnergy = maxEnergy;
         this.currentEnergy = maxEnergy;
     }
+
     // --- Getters & Setters ---
     public int getCurrentEnergy() {
         return currentEnergy;
     }
 
+    private void notifyEnergyChange() {
+        if (entity != null) {
+            entity.getEvents().trigger(EVT_UPDATE_ENERGY, this.currentEnergy);
+        }
+    }
+
     public void setCurrentEnergy(int currentEnergy) {
-        this.currentEnergy = currentEnergy;
+        if (currentEnergy >= 0) {
+            this.currentEnergy = Math.min(currentEnergy, this.maxEnergy);
+        } else {
+            this.currentEnergy = 0;
+        }
+        notifyEnergyChange();
     }
 
     public int getMaxEnergy() {
@@ -29,8 +45,11 @@ public class EnergyComponent {
     }
 
     public void setMaxEnergy(int maxEnergy) {
-        this.maxEnergy = maxEnergy;
+        this.maxEnergy = Math.max(maxEnergy, 1);
+        if (entity != null) {
+            entity.getEvents().trigger(EVT_UPDATE_MAX_ENERGY, this.maxEnergy);
+        }
     }
-
 }
 
+// --- API Stubs for  ---
