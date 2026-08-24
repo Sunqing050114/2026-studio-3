5e395dc6472315f0be520572f85855feff0c9134
Jeyllyrez <Thunthanut.te@gmail.com>
2026-08-24 23:24:35 +1000

    Add max health, damage, healing and death event to CombatStatsComponent
    Player stat are merge into CombatStatsComponent. Any stat that are not shared between the player and the enemy should be created in another component where the player specific stat are extended from the combatStatsComponent.
    
    Max health has a floor of 1, cap of the health are cap at max health. When the entity health is reduce to 0 it fire "entityIsDead" once.

--- source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
+++ source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
@@ -11,12 +11,22 @@ import org.slf4j.LoggerFactory;
 public class CombatStatsComponent extends Component {
 
   private static final Logger logger = LoggerFactory.getLogger(CombatStatsComponent.class);
+  private static final String EVT_IS_DEAD = "entityIsDead";
+  private static final String EVT_MAX_HEALTH = "updateMaxHealth";
   private int health;
   private int baseAttack;
+  private int maxHealth;
 
   public CombatStatsComponent(int health, int baseAttack) {
+    setMaxHealth(health);
     setHealth(health);
     setBaseAttack(baseAttack);
+  }
+
+  private void updateHealth() {
+    if (entity != null) {
+      entity.getEvents().trigger("updateHealth", this.health);
+    }
   }
 
   /**
@@ -25,7 +35,7 @@ public class CombatStatsComponent extends Component {
    * @return is player dead
    */
   public Boolean isDead() {
-    return health == 0;
+    return this.health == 0;
   }
 
   /**
@@ -34,7 +44,16 @@ public class CombatStatsComponent extends Component {
    * @return entity's health
    */
   public int getHealth() {
-    return health;
+    return this.health;
+  }
+
+  /**
+   * Returns the entity's max health.
+   *
+   * @return entity's max health
+   */
+  public int getMaxHealth() {
+    return this.maxHealth;
   }
 
   /**
@@ -44,22 +63,66 @@ public class CombatStatsComponent extends Component {
    */
   public void setHealth(int health) {
     if (health >= 0) {
-      this.health = health;
+      this.health = Math.min(health, this.maxHealth);
     } else {
       this.health = 0;
     }
-    if (entity != null) {
-      entity.getEvents().trigger("updateHealth", this.health);
+    updateHealth();
+  }
+
+  /**
+   * Heals. Health has a maximum bound of the max health
+   *
+   * @param health health
+   */
+  public void heal(int health) {
+    if (health > 0) {
+      setHealth(Math.min(this.health + health, this.maxHealth));
     }
   }
 
   /**
-   * Adds to the player's health. The amount added can be negative.
+   * Damage the entity's health. If health reaches 0 the entity die. currently has a placeholder to
+   * calculate shield before player's health. Unfinished
    *
-   * @param health health to add
+   * @param damage damage
    */
-  public void addHealth(int health) {
-    setHealth(this.health + health);
+  public void takeDamage(int damage) {
+    // add function to calculate damage against shield
+    if (damage >= 0 && !isDead()) {
+      setHealth(Math.max(this.health - damage, 0));
+      if (entity != null && isDead()) {
+        entity.getEvents().trigger("entityIsDead");
+      }
+    }
+  }
+
+  /**
+   * A setter function for maxHealth, contains a safegaurd to avoid MaxHealth going lower than 1
+   * send an update to every listener is changed to ensure real time changes updated.
+   *
+   * @param healthAmount to be set as the MaxHealth
+   */
+  public void setMaxHealth(int healthAmount) {
+    this.maxHealth =
+        Math.max(healthAmount, 1); // Use math.max to restrict the maxhealth from going below 1
+    if (entity != null) {
+      entity
+          .getEvents()
+          .trigger(
+              "updateMaxHealth",
+              this.maxHealth); // this line (basically tells other that is listening to this that
+      // the value is changed)
+    }
+  }
+
+  /**
+   * A function to increase the max health. It uses setMaxHealth function to avoid redundancy
+   *
+   * @param HealthAmount to be set as the MaxHealth
+   */
+  public void addMaxHealth(int healthAmount) {
+    setMaxHealth(healthAmount + this.maxHealth);
   }
 
   /**
@@ -84,6 +147,20 @@ public class CombatStatsComponent extends Component {
     }
   }
 
+  /**
+   * Returns the entity's base attack damage. Unused will remove in later sprint
+   *
+   * @return base attack damage
+   */
+  public void addHealth(int health) {
+    setHealth(this.health + health);
+  }
+
+  /**
+   * Returns the entity's base attack damage. Unused will remove in later sprint
+   *
+   * @return base attack damage
+   */
   public void hit(CombatStatsComponent attacker) {
     int newHealth = getHealth() - attacker.getBaseAttack();
     setHealth(newHealth);
---
+++ source/core/src/main/com/csse3200/game/components/player/PlayerStatsComponent.java
@@ -0,0 +1,4 @@
+package com.csse3200.game.components.player;
+
+import com.csse3200.game.components.Component;
+
