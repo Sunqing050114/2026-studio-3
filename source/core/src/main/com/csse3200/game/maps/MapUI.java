fa988e477b0c252bfd529ee6373b6d8e57d48a5d
peeblesdamian <peeblesdamian@gmail.com>
2026-08-27 00:28:21 +1000

    Attempted to fix format_check_java on previous commit

--- source/core/src/main/com/csse3200/game/maps/MapUI.java
+++ source/core/src/main/com/csse3200/game/maps/MapUI.java
@@ -1,9 +1,5 @@
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.csse3200.game.ui.UIComponent;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.csse3200.game.ui.UIComponent;
 
 public abstract class MapUI extends UIComponent {
 
