70884db3ae56c5d4c4f819519487290b95e0e927
Zaidan Alphard <zachalp@Zaidan-Alphard.local>
2026-08-26 14:34:00 +1000

    refactor(maps): rework node selection against real MapGraph

--- source/core/src/main/com/csse3200/game/components/maps/NodeState.java
+++
@@ -1,8 +0,0 @@
-package com.csse3200.game.components.maps;
-
-public enum NodeState {
-    LOCKED,
-    AVAILABLE,
-    CURRENT,
-    COMPLETED
-}
--- source/core/src/main/com/csse3200/game/components/maps/RoomType.java
+++
@@ -1,8 +0,0 @@
-package com.csse3200.game.components.maps;
-
-public enum RoomType {
-    COMBAT,
-    SHOP,
-    EVENT,
-    FINAL
-}
