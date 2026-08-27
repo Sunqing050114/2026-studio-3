9d9677442b6356c99281d3c4c3d75ed03f23eb02
xiaojaxon <xiaojaxon@gmail.com>
2026-08-28 03:43:33 +1000

    Add enemy armour damage test (#19)

---
+++ source/core/src/test/com/csse3200/game/components/enemy/EnemyStatsComponentTest.java
@@ -0,0 +1,24 @@
+package com.csse3200.game.components.enemy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.extensions.GameExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(GameExtension.class)
+class EnemyStatsComponentTest {
+  @Test
+  void shouldApplyDamageToArmourBeforeHealth() {
+    Entity enemy = new Entity();
+    EnemyStatsComponent stats = new EnemyStatsComponent(20, 6, 5);
+    enemy.addComponent(stats);
+    enemy.create();
+
+    stats.takeDamage(8);
+
+    assertEquals(17, stats.getHealth());
+    assertEquals(0, stats.getArmour());
+  }
+}
