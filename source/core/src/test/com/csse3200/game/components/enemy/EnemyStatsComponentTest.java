79ea70adaf8f4ad9e3e887c96f566d81deb57a92
xiaojaxon <xiaojaxon@gmail.com>
2026-08-28 03:52:01 +1000

    Add enemy armour damage tests (#19)

--- source/core/src/test/com/csse3200/game/components/enemy/EnemyStatsComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/enemy/EnemyStatsComponentTest.java
@@ -21,4 +21,16 @@ class EnemyStatsComponentTest {
     assertEquals(17, stats.getHealth());
     assertEquals(0, stats.getArmour());
   }
+  @Test
+  void shouldLetArmourFullyAbsorbDamage() {
+    Entity enemy = new Entity();
+    EnemyStatsComponent stats = new EnemyStatsComponent(20, 6, 5);
+    enemy.addComponent(stats);
+    enemy.create();
+
+    stats.takeDamage(3);
+
+    assertEquals(20, stats.getHealth());
+    assertEquals(2, stats.getArmour());
+  }
 }
