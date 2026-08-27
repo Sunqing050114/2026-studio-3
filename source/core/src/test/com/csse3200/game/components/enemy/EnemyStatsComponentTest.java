266132e1c9a39ce600e7b375183a41c06e4e1e56
xiaojaxon <xiaojaxon@gmail.com>
2026-08-28 04:26:26 +1000

    Add enemy stats event and armour tests (#19)

--- source/core/src/test/com/csse3200/game/components/enemy/EnemyStatsComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/enemy/EnemyStatsComponentTest.java
@@ -1,11 +1,15 @@
 package com.csse3200.game.components.enemy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.extensions.GameExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import com.csse3200.game.events.listeners.EventListener0;
+import com.csse3200.game.events.listeners.EventListener1;
 
 @ExtendWith(GameExtension.class)
 class EnemyStatsComponentTest {
@@ -72,5 +76,74 @@ class EnemyStatsComponentTest {
 
     assertEquals(20, stats.getHealth());
     assertEquals(5, stats.getArmour());
+  }
+
+  @Test
+  void shouldNotReduceHealthBelowZero() {
+    Entity enemy = new Entity();
+    EnemyStatsComponent stats = new EnemyStatsComponent(20, 6, 0);
+    enemy.addComponent(stats);
+    enemy.create();
+
+    stats.takeDamage(999);
+
+    assertEquals(0, stats.getHealth());
+  }
+
+  @Test
+  void shouldTriggerEnemyDefeatedWhenHealthReachesZero() {
+    Entity enemy = new Entity();
+    EnemyStatsComponent stats = new EnemyStatsComponent(20, 6, 0);
+    enemy.addComponent(stats);
+    enemy.create();
+
+    EventListener0 listener = mock(EventListener0.class);
+    enemy.getEvents().addListener("enemyDefeated", listener);
+
+    stats.takeDamage(20);
+
+    verify(listener).handle();
+  }
+
+  @Test
+  void shouldTriggerEnemyDamagedWithActualHealthDamage() {
+    Entity enemy = new Entity();
+    EnemyStatsComponent stats = new EnemyStatsComponent(20, 6, 5);
+    enemy.addComponent(stats);
+    enemy.create();
+
+    @SuppressWarnings("unchecked")
+    EventListener1<Integer> listener = (EventListener1<Integer>) mock(EventListener1.class);
+    enemy.getEvents().addListener("enemyDamaged", listener);
+
+    stats.takeDamage(8);
+
+    verify(listener).handle(3);
+    assertEquals(17, stats.getHealth());
+    assertEquals(0, stats.getArmour());
+  }
+
+  @Test
+  void shouldClampSetArmourToZero() {
+    Entity enemy = new Entity();
+    EnemyStatsComponent stats = new EnemyStatsComponent(20, 6, 5);
+    enemy.addComponent(stats);
+    enemy.create();
+
+    stats.setArmour(-10);
+
+    assertEquals(0, stats.getArmour());
+  }
+
+  @Test
+  void shouldClampAddArmourToZero() {
+    Entity enemy = new Entity();
+    EnemyStatsComponent stats = new EnemyStatsComponent(20, 6, 5);
+    enemy.addComponent(stats);
+    enemy.create();
+
+    stats.addArmour(-10);
+
+    assertEquals(0, stats.getArmour());
   }
 }
