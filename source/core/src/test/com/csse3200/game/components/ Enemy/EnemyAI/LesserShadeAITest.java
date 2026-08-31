bb3691d0949943265b0f1837eeea0fa8074c8a15
LIWANGXIN <sober137756@gmail.com>
2026-08-31 13:26:40 +1000

    change the format

--- source/core/src/test/com/csse3200/game/components/ Enemy/EnemyAI/LesserShadeAITest.java
+++ source/core/src/test/com/csse3200/game/components/ Enemy/EnemyAI/LesserShadeAITest.java
@@ -7,36 +7,36 @@ import com.csse3200.game.components.enemy.IntentType;
 import org.junit.jupiter.api.Test;
 
 class LesserShadeAITest {
-    private final LesserShadeAI ai = new LesserShadeAI();
+  private final LesserShadeAI ai = new LesserShadeAI();
 
-    @Test
-    void shouldAttackOnOddTurn() {
-        EnemyAIContext context = createContext(1);
+  @Test
+  void shouldAttackOnOddTurn() {
+    EnemyAIContext context = createContext(1);
 
-        EnemyIntent intent = ai.decide(context);
+    EnemyIntent intent = ai.decide(context);
 
-        assertEquals(IntentType.ATTACK, intent.getType());
-        assertEquals(6, intent.getValue());
-    }
+    assertEquals(IntentType.ATTACK, intent.getType());
+    assertEquals(6, intent.getValue());
+  }
 
-    @Test
-    void shouldDefendOnEvenTurn() {
-        EnemyAIContext context = createContext(2);
+  @Test
+  void shouldDefendOnEvenTurn() {
+    EnemyAIContext context = createContext(2);
 
-        EnemyIntent intent = ai.decide(context);
+    EnemyIntent intent = ai.decide(context);
 
-        assertEquals(IntentType.DEFEND, intent.getType());
-        assertEquals(2, intent.getValue());
-    }
+    assertEquals(IntentType.DEFEND, intent.getType());
+    assertEquals(2, intent.getValue());
+  }
 
-    private EnemyAIContext createContext(int turnNumber) {
-        return new EnemyAIContext(
-                100,                   // playerHealth
-                24,                    // enemyHealth
-                24,                    // enemyMaxHealth
-                6,                     // enemyAttack
-                0,                     // enemyArmour
-                EnemyIntent.unknown(), // previousIntent
-                turnNumber);
-    }
+  private EnemyAIContext createContext(int turnNumber) {
+    return new EnemyAIContext(
+        100, // playerHealth
+        24, // enemyHealth
+        24, // enemyMaxHealth
+        6, // enemyAttack
+        0, // enemyArmour
+        EnemyIntent.unknown(), // previousIntent
+        turnNumber);
+  }
 }
